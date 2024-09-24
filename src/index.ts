53bb4704b6f6a5d600daaa35a477a348b011c99b
mckennacs <csmckenna13@gmail.com>
2024-09-24 00:39:59 -0700

    post test complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText, { dryRun: true });
+// const text = await Bot.run(getPostText, { dryRun: true });
+const text = await Bot.run(getPostText);
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
